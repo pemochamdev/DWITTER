482f1f2a2a32caec5d5f86a1e30254c24731a931
pemochamdev <pemochamdev@gmail.com>
2023-10-16 04:12:35 +0000

    integration de bulma et creation de quelques templates comme le base.html, profile.html et profile_list.html, ecriture du controleur views avec des fonctions telles que la profile_list et profile et dashboard

--- dwitter/urls.py
+++ dwitter/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from dwitter import views
 
 urlpatterns = [
-    path('')
+    path('', views.dashboard, name='dashboard'),
+        path("profile_list/", views.profile_list, name="profile_list"),
+        path("profile/<int:pk>", views.profile_views, name="profile"),
+
 ]
--- dwitter/views.py
+++ dwitter/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from dwitter.models import Profile
+
+def dashboard(request):
+    return render(request, 'base.html')
+
+
+def profile_list(request):
+    user = request.user
+    profiles = Profile.objects.exclude(user = user)
+
+    context = {
+        'profiles':profiles
+    }
+    return render(request, 'dwitter/profile_list.html', context=context)
+
+def profile_views(request, pk):
+    profile = get_object_or_404(Profile, pk=pk)
+    context = {
+        'profile':profile,
+    }
+    return render(request, 'dwitter/profile.html', context=context)
